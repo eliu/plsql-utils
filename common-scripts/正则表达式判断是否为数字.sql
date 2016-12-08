37ca961c7b0f586a541b5f865edb34281055bab5
刘洪玉 <memories.liu@hand-china.com>
2016-12-08 15:30:15 +0800

    完善数字判断的逻辑

--- common-scripts/正则表达式判断是否为数字.sql
+++ common-scripts/正则表达式判断是否为数字.sql
@@ -1,27 +1,47 @@
 -- Data source begin
-WITH DATA_SOURCE AS (
-SELECT '1' COL1  FROM DUAL
+WITH data_source AS (
+SELECT '1' number_like_string  FROM DUAL    -- good
 UNION ALL
-SELECT 'ABC'       FROM DUAL
+SELECT '100.4'     FROM DUAL  -- good
 UNION ALL
-SELECT '234234ABC' FROM DUAL
+SELECT '-100.4'    FROM DUAL  -- good
 UNION ALL
-SELECT '100.4'     FROM DUAL
+SELECT '.1234'     FROM DUAL  -- good
 UNION ALL
-SELECT 'ABC'       FROM DUAL
+SELECT '-.1234'    FROM DUAL  -- good
 UNION ALL
-SELECT '-100.4'    FROM DUAL
+SELECT '1234.'     FROM DUAL  -- good
 UNION ALL
-SELECT '.1234'     FROM DUAL
+SELECT '+1234.56'  FROM DUAL  -- good
 UNION ALL
-SELECT '-.1234'    FROM DUAL
+SELECT 'ABC'       FROM DUAL  -- NG
 UNION ALL
-SELECT '1234.'     FROM dual
+SELECT '234234ABC' FROM DUAL  -- NG
 UNION ALL
-SELECT '.'     FROM DUAL)
+SELECT '.'     FROM DUAL)     -- NG
 -- Data source end
-SELECT ds.col1,
-       regexp_substr(ds.col1,'^[\.\+-]?((\d+(\.\d*)?)|(\d*\.\d+))$') col2
-  FROM data_source ds
- WHERE REGEXP_SUBSTR(DS.COL1,'^[\.\+-]?((\d+(\.\d*)?)|(\d*\.\d+))$') IS NOT NULL
-;
+SELECT ds.number_like_string,
+       decode(regexp_substr(ds.number_like_string,'^[\.\+-]?((\d+(\.\d*)?)|(\d*\.\d+))$'),
+              null, 'NG!', 'Good') MATCHED_RESULT
+  FROM data_source ds;
+/*
++=================+
+| Expected Result |
++=================+
+NUMBER_LI MATC
+--------- ----
+1         Good
+100.4     Good
+-100.4    Good
+.1234     Good
+-.1234    Good
+1234.     Good
++1234.56  Good
+ABC       NG!
+234234ABC NG!
+.         NG!
+
+已选择 10 行。
+
+SQL>
+*/
